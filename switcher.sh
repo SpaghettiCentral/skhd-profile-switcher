9c4a47a7e8b8fc49f8dd6418bbc7468233bd6055
Sebastian Stubenvoll <sebastian.stubenvoll@gmail.com>
2020-07-21 17:40:39 +0200

    error check

--- switcher.sh
+++ switcher.sh
@@ -3,11 +3,13 @@
 CURRENT_CFG_NAME=$(head -1 ${SKHD_DIR}/skhdrc | cut -c2-)
 NEW_CFG=$1
 
-mv "${SKHD_DIR}/skhdrc" "${SKHD_DIR}/${CURRENT_CFG_NAME}"
-mv "${SKHD_DIR}/$1" "${SKHD_DIR}/skhdrc"
-
-osascript -e 'display notification "changed SKHD config to '"$(head -1 ${SKHD_DIR}/skhdrc | cut -c2-)"'" with title "SKHD config"' 
-
+if [ -f "${SKHD_DIR}/$1" ]; then
+	mv "${SKHD_DIR}/skhdrc" "${SKHD_DIR}/${CURRENT_CFG_NAME}"
+	mv "${SKHD_DIR}/$1" "${SKHD_DIR}/skhdrc"
+	osascript -e 'display notification "changed SKHD config to '"$(head -1 ${SKHD_DIR}/skhdrc | cut -c2-)"'" with title "SKHD config"' 
+else
+	osascript -e 'display notification "no such config '"$1"'" with title "SKHD config"'
+fi
 
 
 
